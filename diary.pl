4509b8901f135c5cb5320af2f572d0d13acf6fbc
nobuoka <nobuoka@r-definition.com>
2012-02-20 09:59:01 +0900

    テキストエディタを用いた内容変更方法を考えた

--- diary.pl
+++ diary.pl
@@ -15,8 +15,8 @@ print "あいうえお\n";
 warn  "警告！\n";
 
 # コマンドライン引数
-my $command = shift @ARGV;
-print length $command, "\n";
+#my $command = shift @ARGV;
+#print length $command, "\n";
 
 # DB の設定ファイルの位置
 my $dbconfpath = "$FindBin::Bin/conf/db.conf";
@@ -40,5 +40,35 @@ my $users = Diary::MoCo::User->search();
 $users->each( sub {
   print $_->name, "\n";
 } );
+
+###
+# 既存の日記の内容をユーザーに編集させる
+#
+use File::Temp;
+use Fcntl qw( :flock );
+my $fh_dialy_body = File::Temp->new();
+binmode $fh_dialy_body, ':encoding(UTF-8)';
+
+# ファイルに書き込み
+flock $fh_dialy_body, LOCK_EX;
+print $fh_dialy_body '既存ファイルの内容', "\n";
+flock $fh_dialy_body, LOCK_UN;
+
+# テキストエディタを開いてユーザーに編集させる
+system( '/usr/bin/vim', $fh_dialy_body->filename ) == 0
+    or die 'fail...';
+
+# 編集後の内容を得る
+flock $fh_dialy_body, LOCK_SH;
+$fh_dialy_body->seek( 0, SEEK_SET );
+my @new_dialy_body_lines = <$fh_dialy_body>;
+flock $fh_dialy_body, LOCK_UN;
+$fh_dialy_body->close();
+
+foreach my $line ( @new_dialy_body_lines ) {
+    print $line;
+}
+
+print '__end__', "\n";
 
 exit 0;
