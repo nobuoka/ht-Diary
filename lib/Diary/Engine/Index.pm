579adcdd5c9a07a7101c028470700cf7abf3c613
nobuoka <nobuoka@r-definition.com>
2012-02-27 23:54:03 +0900

    Added feature to show article list

--- lib/Diary/Engine/Index.pm
+++ lib/Diary/Engine/Index.pm
@@ -2,11 +2,21 @@ package Diary::Engine::Index;
 use strict;
 use warnings;
 use Diary::Engine -Base;
+use Diary::MoCo::User;
 
 sub default : Public {
     my ($self, $r) = @_;
-    $r->res->content_type('text/plain');
-    $r->res->content('Welcome to the Ridge world!');
+
+    # とりあえずユーザー名は決めうち
+    my $user = Diary::MoCo::User->find( name => 'nobuoka' )
+        or die "can't find you on database. please do userconf command at first.";
+    my $articles = $user->articles;
+    $r->stash->param(
+        user     => $user,
+        articles => $articles,
+    );
+    #$r->res->content_type('text/plain');
+    #$r->res->content('Welcome to the Ridge world!');
 }
 
 1;
