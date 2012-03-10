c9f5169b0bd996e9af82cf3f9dbf6da361d36dd7
nobuoka <nobuoka@r-definition.com>
2012-03-10 21:11:09 +0900

    Added feature to edit article
    記事を更新するための METHOD - URI ペアとして,
      POST - /user:{user_name}/article:{article_id};update
    を, 記事編集画面として
      GET - /user:{user_name}/article:{article_id};edit
    を導入.

--- lib/Diary/Engine/User/Article.pm
+++ lib/Diary/Engine/User/Article.pm
@@ -40,12 +40,142 @@ sub _get {
         return;
     }
 
-   $r->stash->param(
+    $r->stash->param(
         user    => $user,
         article => $article,
     );
-    #$r->res->content_type('text/plain');
-    #$r->res->content('Welcome to the Ridge world!');
+}
+
+sub edit : Public {
+    my ( $self, $r ) = @_;
+    return $r->follow_method;
+}
+sub _edit_get {
+    my ($self, $r) = @_;
+
+    # ログインユーザー
+    my $auth_user = $r->user;
+    if ( ! defined $auth_user ) {
+        $r->res->code( '401' );
+        $r->res->content( '401 Unauthorized' );
+        return;
+
+    }
+
+    # URI params
+    my $article_id = $r->req->uri->param('article_id');
+    my $user_name  = $r->req->uri->param('user_name');
+    if ( ! defined $article_id ) {
+        # 例外
+        $r->res->code( '404' );
+        $r->res->content( '404 NOT FOUND' );
+        return;
+    }
+    if ( ! defined $user_name ) {
+        # 例外
+        $r->res->code( '404' );
+        $r->res->content( '404 NOT FOUND' );
+        return;
+    }
+    if ( $user_name != $auth_user->name ) {
+        $r->res->code( '403' );
+        $r->res->content( '403 Forbidden' );
+        return;
+    }
+
+    # User オブジェクトの取得; 失敗の場合は 404 エラー
+    my $user = Diary::MoCo::User->find( name => $user_name );
+    if ( ! defined $user ) {
+        # 例外
+        $r->res->code( '404' );
+        $r->res->content( '404 NOT FOUND' );
+        return;
+    }
+
+    # Article オブジェクトの取得; 失敗の場合は 404 エラー
+    my $article = $user->select_article_by_id( $article_id );
+    if ( ! defined $article ) {
+        # 例外
+        $r->res->code( '404' );
+        $r->res->content( '404 NOT FOUND' );
+        return;
+    }
+
+    $r->stash->param(
+        user    => $user,
+        article => $article,
+    );
+} 
+
+sub update : Public {
+    my ( $self, $r ) = @_;
+    return $r->follow_method;
+}
+sub _update_post {
+    my ($self, $r) = @_;
+
+    # ログインユーザー
+    my $auth_user = $r->user;
+    if ( ! defined $auth_user ) {
+        $r->res->code( '401' );
+        $r->res->content( '401 Unauthorized' );
+        return;
+
+    }
+
+    # URI params
+    my $article_id = $r->req->uri->param('article_id');
+    my $user_name  = $r->req->uri->param('user_name');
+    if ( ! defined $article_id ) {
+        # 例外
+        $r->res->code( '404' );
+        $r->res->content( '404 NOT FOUND' );
+        return;
+    }
+    if ( ! defined $user_name ) {
+        # 例外
+        $r->res->code( '404' );
+        $r->res->content( '404 NOT FOUND' );
+        return;
+    }
+
+    # body params
+    my $body  = $r->req->body_parameters->get( 'body'  );
+    my $title = $r->req->body_parameters->get( 'title' );
+    if ( ! defined $title ) {
+        # 例外
+        $r->res->code( '400' );
+        $r->res->content( '400 BAD REQUEST : no title' );
+        return;
+    }
+    if ( ! defined $body ) {
+        # 例外
+        $r->res->code( '400' );
+        $r->res->content( '400 BAD REQUEST : no body' );
+        return;
+    }
+ 
+    # User オブジェクトの取得; 失敗の場合は 404 エラー
+    my $user = Diary::MoCo::User->find( name => $user_name );
+    if ( ! defined $user ) {
+        # 例外
+        $r->res->code( '404' );
+        $r->res->content( '404 NOT FOUND' );
+        return;
+    }
+
+    # Article オブジェクトの取得; 失敗の場合は 404 エラー
+    my $article = $user->select_article_by_id( $article_id );
+    if ( ! defined $article ) {
+        # 例外
+        $r->res->code( '404' );
+        $r->res->content( '404 NOT FOUND' );
+        return;
+    }
+    $article->param( 'body',  $body  );
+    $article->param( 'title', $title );
+    $r->res->redirect( '/user:' . $user_name . '/article:' . $article_id );
+    return;
 }
 
 sub delete : Public {
